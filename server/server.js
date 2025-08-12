42f4e03c165e98ef320058493cbd203037ccf733
montaser-99 <hicishicis112233@gmail.com>
2025-08-12 06:34:27 +0300

    Prepare backend for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -1,28 +1,28 @@
-import { Dbconnection } from "./config/mongoose.js"
-import express from"express"
-import dotenv from"dotenv"
-dotenv.config()
-import cookieParser from "cookie-parser"
-import helmet from "helmet"
-import morgan from "morgan"
-import cors from"cors"
-import userRouter from "./routes/userroutes.js"
-import categoryRouter from "./routes/categoryroutes.js"
-import uploadRouter from "./routes/uploadrouter.js"
-import SubCategoryRouter from "./routes/subcategoryroutes.js"
-import productRouter from "./routes/productroutes.js"
-import CartRouter from "./routes/cartroutes.js"
-import AddressRouter from "./routes/addressroutes.js"
-import orderRouter from "./routes/orderroutes.js"
-import { webhookStripe } from "./controllers/order.controller.js"
+import { Dbconnection } from "./config/mongoose.js";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
 
+import userRouter from "./routes/userroutes.js";
+import categoryRouter from "./routes/categoryroutes.js";
+import uploadRouter from "./routes/uploadrouter.js";
+import SubCategoryRouter from "./routes/subcategoryroutes.js";
+import productRouter from "./routes/productroutes.js";
+import CartRouter from "./routes/cartroutes.js";
+import AddressRouter from "./routes/addressroutes.js";
+import orderRouter from "./routes/orderroutes.js";
+import { webhookStripe } from "./controllers/order.controller.js";
 
-const app=express()
-const Port=process.env.PORT||5000
+const app = express();
 
 app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(morgan("dev"));
+
 app.post(
   "/webhook",
   express.raw({ type: "application/json" }),
@@ -31,40 +31,23 @@ app.post(
 app.use(express.json());
 app.use(cookieParser());
 
+// Routes
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+app.use("/api/user", userRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/sub-category", SubCategoryRouter);
+app.use("/api/upload", uploadRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", CartRouter);
+app.use("/api/address", AddressRouter);
+app.use("/api/order", orderRouter);
 
+// Connect DB
+Dbconnection();
 
-
-//ROUTES
-// app.get("/", (req, res) => {
-//   res.send("API is running...");
-// }); 
-app.use('/api/user',userRouter)
-app.use('/api/category',categoryRouter)
-app.use('/api/sub-category',SubCategoryRouter)
-app.use('/api/upload',uploadRouter)
-app.use('/api/product',productRouter)
-app.use('/api/cart',CartRouter)
-app.use('/api/address',AddressRouter)
-app.use('/api/order',orderRouter)
- 
-
-
-
-
-Dbconnection().then(()=>{
-    app.listen(Port,()=>{
-        console.log(`app is running on port :${Port}`)
-    })
-})
-
-
-
-
-
-
-
-
-
+export default app;
 
 
 
